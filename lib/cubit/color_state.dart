d7b532ec54662b744b01d46c8cffbb97f2905be6
Eleandro Duzentos <eleandro@inbox.ru>
2020-11-29 18:39:33 +0100

    Fixed initial hexadecimal color opacity

--- lib/cubit/color_state.dart
+++ lib/cubit/color_state.dart
@@ -2,7 +2,7 @@ part of 'color_cubit.dart';
 
 @freezed
 abstract class ColorState with _$ColorState {
-  const factory ColorState.initial([@Default(Color(0x00FFFFFF)) Color color]) =
+  const factory ColorState.initial([@Default(Color(0xFFFFFFFF)) Color color]) =
       _Initial;
   const factory ColorState.color(Color color) = _Changed;
 }
