4baa40ba17a084423e572873447f787b9c3a2c4f
Eleandro Duzentos <eleandro.duzentos@ideiasdinamicas.com>
2020-12-05 16:44:36 +0100

    Fixed issue with ColorTextField rebuilding when text changed

--- lib/screens/home/widgets/color_text_field.dart
+++ lib/screens/home/widgets/color_text_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ColorTextField extends StatelessWidget {
+class ColorTextField extends StatefulWidget {
   final Color color;
   final String value;
   final double fontSize;
@@ -15,11 +15,23 @@ class ColorTextField extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _ColorTextFieldState createState() => _ColorTextFieldState();
+}
+
+class _ColorTextFieldState extends State<ColorTextField> {
+  final _textFieldFocusNode = FocusNode();
+
+  Key _textFieldKey = UniqueKey();
+
+  @override
   Widget build(BuildContext context) {
+    _textFieldKey = _textFieldFocusNode.hasFocus ? _textFieldKey : UniqueKey();
+
     return TextFormField(
+      key: _textFieldKey,
       textAlign: TextAlign.center,
-      key: UniqueKey(),
-      initialValue: value,
+      focusNode: _textFieldFocusNode,
+      initialValue: widget.value,
       decoration: InputDecoration(
         isDense: true,
         border: InputBorder.none,
@@ -27,12 +39,12 @@ class ColorTextField extends StatelessWidget {
       style: TextStyle(
         color: idealColor,
         fontWeight: FontWeight.bold,
-        fontSize: fontSize,
+        fontSize: widget.fontSize,
       ),
-      onChanged: onChanged,
+      onChanged: widget.onChanged,
     );
   }
 
   Color get idealColor =>
-      color.computeLuminance() > .5 ? Colors.black : Colors.white;
+      widget.color.computeLuminance() > .5 ? Colors.black : Colors.white;
 }
