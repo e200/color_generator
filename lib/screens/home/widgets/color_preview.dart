6d02a3ec9b364fc5ef070fc37841d2b68f1e3c09
Eleandro Duzentos <eleandro.duzentos@ideiasdinamicas.com>
2020-12-05 23:40:11 +0100

    Added color preview

---
+++ lib/screens/home/widgets/color_preview.dart
@@ -0,0 +1,14 @@
+import 'package:color_generator/cubit/color_cubit.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ColorPreview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _color = context.watch<ColorCubit>().state.color;
+
+    return Container(
+      color: _color.withOpacity(1),
+    );
+  }
+}
