c5fa80d8b6f61be9e2832e467fcbbad8da6ba74b
Eleandro Duzentos <eleandro.duzentos@ideiasdinamicas.com>
2020-12-05 23:40:00 +0100

    Removed HomeScreen watch that causes the entire screen to rebuild on ColorState changes

--- lib/screens/home/screen.dart
+++ lib/screens/home/screen.dart
@@ -1,4 +1,5 @@
 import 'package:color_generator/cubit/color_cubit.dart';
+import 'package:color_generator/screens/home/widgets/color_preview.dart';
 import 'package:color_generator/screens/home/widgets/color_sliders.dart';
 import 'package:color_generator/screens/home/widgets/hexadecimal_color_text_field.dart';
 import 'package:flutter/cupertino.dart';
@@ -8,32 +9,32 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final _color = context.watch<ColorCubit>().state.color;
-
     return Scaffold(
       body: Stack(
         children: [
-          Container(color: _color.withOpacity(1)),
-          SafeArea(
-            child: Center(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      HexadecimalColorTextField(
-                        color: _color,
-                        onChanged: (String hexadecimal) {
-                          context
-                              .read<ColorCubit>()
-                              .updateHexadecimal(hexadecimal);
-                        },
-                      ),
-                      SizedBox(height: 60),
-                      ColorSliders(),
-                    ],
-                  ),
+          ColorPreview(),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    BlocBuilder<ColorCubit, ColorState>(
+                      builder: (context, state) {
+                        return HexadecimalColorTextField(
+                          color: state.color,
+                          onChanged: (String hexadecimal) {
+                            context
+                                .read<ColorCubit>()
+                                .updateHexadecimal(hexadecimal);
+                          },
+                        );
+                      },
+                    ),
+                    SizedBox(height: 60),
+                    ColorSliders(),
+                  ],
                 ),
               ),
             ),
