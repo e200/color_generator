b6cf144b4ac2cf49c9bfd459315c0a1dfd734b57
Eleandro Duzentos <eleandro.duzentos@ideiasdinamicas.com>
2020-12-06 02:33:32 +0100

    Added SafeArea to components in front of the background

--- lib/screens/home/screen.dart
+++ lib/screens/home/screen.dart
@@ -2,6 +2,7 @@ import 'package:color_generator/cubit/color_cubit.dart';
 import 'package:color_generator/screens/home/widgets/color_preview.dart';
 import 'package:color_generator/screens/home/widgets/color_sliders.dart';
 import 'package:color_generator/screens/home/widgets/hexadecimal_color_text_field.dart';
+import 'package:color_generator/screens/home/widgets/top_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,30 +14,37 @@ class HomeScreen extends StatelessWidget {
       body: Stack(
         children: [
           ColorPreview(),
-          Center(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    BlocBuilder<ColorCubit, ColorState>(
-                      builder: (context, state) {
-                        return HexadecimalColorTextField(
-                          color: state.color,
-                          onChanged: (String hexadecimal) {
-                            context
-                                .read<ColorCubit>()
-                                .updateHexadecimal(hexadecimal);
-                          },
-                        );
-                      },
+          SafeArea(
+            child: Column(
+              children: [
+                TopBar(),
+                Center(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          BlocBuilder<ColorCubit, ColorState>(
+                            builder: (context, state) {
+                              return HexadecimalColorTextField(
+                                color: state.color,
+                                onChanged: (String hexadecimal) {
+                                  context
+                                      .read<ColorCubit>()
+                                      .updateHexadecimal(hexadecimal);
+                                },
+                              );
+                            },
+                          ),
+                          SizedBox(height: 60),
+                          ColorSliders(),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 60),
-                    ColorSliders(),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         ],
