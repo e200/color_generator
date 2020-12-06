42540b4cfe68f622aaa7e834b1001e3126e46c72
Eleandro Duzentos <eleandro.duzentos@ideiasdinamicas.com>
2020-12-06 02:34:36 +0100

    Added _getImage method

--- lib/screens/home/widgets/top_bar.dart
+++ lib/screens/home/widgets/top_bar.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:color_generator/cubit/color_cubit.dart';
+import 'package:color_generator/screens/image_color_picker/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TopBar extends StatelessWidget {
   const TopBar({
@@ -21,6 +26,23 @@ class TopBar extends StatelessWidget {
               final _source = await _getImageSource(context);
 
               final _image = await _getImage(context, _source);
+
+              if (_image != null) {
+                final _color = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ImageColorPickerScreen(
+                        image: _image,
+                      );
+                    },
+                  ),
+                );
+
+                if (_color != null) {
+                  context.read<ColorCubit>().updateColor(_color);
+                }
+              }
             },
           ),
           /* IconButton(
@@ -31,32 +53,6 @@ class TopBar extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  _getImage(BuildContext context, ImageSource source) async {
-    if (source != null) {
-      final _imagePicker = ImagePicker();
-
-      try {
-        await _imagePicker.getImage(source: source);
-      } on PlatformException catch (_) {
-        final _snackbar = SnackBar(
-          content: Text('Permission to read gallery denied'),
-          backgroundColor: Colors.red,
-          behavior: SnackBarBehavior.floating,
-          duration: Duration(seconds: 5),
-          action: SnackBarAction(
-            label: 'Try again',
-            textColor: Colors.white,
-            onPressed: () {
-              _getImage(context, source);
-            },
-          ),
-        );
-
-        Scaffold.of(context).showSnackBar(_snackbar);
-      }
-    }
   }
 
   Future<ImageSource> _getImageSource(BuildContext context) async {
@@ -86,5 +82,36 @@ class TopBar extends StatelessWidget {
     );
 
     return _option;
+  }
+
+  // TODO: Move this code into its own Cubit
+  Future<File> _getImage(BuildContext context, ImageSource source) async {
+    if (source != null) {
+      final _imagePicker = ImagePicker();
+
+      try {
+        final _pickedImage = await _imagePicker.getImage(source: source);
+
+        return File(_pickedImage.path);
+      } on PlatformException catch (_) {
+        final _snackbar = SnackBar(
+          content: Text('Permission to read gallery denied'),
+          backgroundColor: Colors.red,
+          behavior: SnackBarBehavior.floating,
+          duration: Duration(seconds: 5),
+          action: SnackBarAction(
+            label: 'Try again',
+            textColor: Colors.white,
+            onPressed: () {
+              _getImage(context, source);
+            },
+          ),
+        );
+
+        Scaffold.of(context).showSnackBar(_snackbar);
+      }
+    }
+
+    return null;
   }
 }
