9add93efdeca0360e5db0f3241695e870565f77f
Eleandro Duzentos <eleandro@inbox.ru>
2021-05-05 22:22:49 +0100

    refactor: delete color cubit

--- lib/cubit/color_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'color_cubit.dart';
-
-@freezed
-abstract class ColorState with _$ColorState {
-  const factory ColorState.initial([@Default(Color(0xFFFFFFFF)) Color color]) =
-      _Initial;
-  const factory ColorState.color(Color color) = _Changed;
-}
